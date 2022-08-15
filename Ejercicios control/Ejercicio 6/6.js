04a78b30ee8137a04cc146ac35db80fb67311a50
Alvaro470515 <alvarin248@gmail.com>
2022-08-15 15:14:36 -0500

    alvaro
    cambio puntos 6

--- Ejercicios control/Ejercicio 6/6.js
+++ Ejercicios control/Ejercicio 6/6.js
@@ -1,11 +1,9 @@
 function multiplicar(){
-	var num1 = parseFloat(document.getElementsByTagName('input')[0].value), num2 = parseFloat(document.getElementsByTagName("input")[1].value);
-	if (num1 > 100 || num2 > 100){
-		alert("No puede ingresar números con más de 3 dígitos");
-        return;
-    }
+	var num1 = parseFloat(document.getElementsByTagName('input')[0].value), 
+	num2 = parseFloat(document.getElementsByTagName("input")[1].value);
+	
 	var multiplicacion = 0;
-	const primer_numero = num1; //No es necesaria, sólo para el alert
+	const primer_numero = num1; 
 	if (num1 > 0)
 	while(num1){
 		multiplicacion += num2;
